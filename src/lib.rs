1fe8eead432649bd9008c0e42c0aff6c15e3e30a
X-some-X <x-some-x@proton.me>
2025-03-04 16:25:07 +0100

    .

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![no_std]
 
-pub use display_text_interface::DisplayText;
+pub use display_text_interface::{DisplayText, ReturnMode};
 
 pub use core::fmt::Write;
 
@@ -46,4 +46,12 @@ macro_rules! set_color_default {
 #[macro_export]
 macro_rules! fill {
     ($r: expr, $v: expr, $b: expr) => {DISPLAY_TEXT.lock().fill(($r, $v, $b))};
+}
+
+#[macro_export]
+macro_rules! move_cursor {
+    (left $count:expr) => {DISPLAY_TEXT.lock().move_left(count, display_text::ReturnMode::None)};
+    (right $count:expr) => {DISPLAY_TEXT.lock().move_right(count, display_text::ReturnMode::None)};
+    (up $count:expr) => {DISPLAY_TEXT.lock().move_up(count, display_text::ReturnMode::None)};
+    (down $count:expr) => {DISPLAY_TEXT.lock().move_down(count, display_text::ReturnMode::None)};
 }
